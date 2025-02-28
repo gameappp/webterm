8ba581a65e64ff43065fdd836f8422f9f6ab0748
shahyad <shahyad.hack@gmail.com>
2025-02-28 11:15:58 +0330

    project configed

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,10 +2,14 @@ import "./globals.css";
 import "../css/styles.css";
 import { Providers } from "./providers";
 
+export const metadata ={
+  title:"Chess Game"
+}
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={""}>
+    <html lang="fa" dir="rtl" className="dark">
+      <body className="max-w-[412px] w-full">
         <Providers>{children}</Providers>
       </body>
     </html>
