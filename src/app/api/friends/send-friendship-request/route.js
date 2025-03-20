9a03d0c9c906412d0eecfc406fe3702554625981
shahyad <shahyad.hack@gmail.com>
2025-03-20 23:49:38 +0330

    send friendship req api

---
+++ src/app/api/friends/send-friendship-request/route.js
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/lib/db";
+import FriendRequest from "@/models/FriendRequest";
+
+export async function POST(request) {
+  try {
+    await connectDB();
+
+    const { senderId, receiverId } = await request.json();
+
+    if (!senderId || !receiverId) {
+      return NextResponse.json(
+        { message: "اطلاعات ناقص است." },
+        { status: 400 }
+      );
+    }
+
+    // check if friendship request exist
+    const existingRequest = await FriendRequest.findOne({
+      sender: senderId,
+      receiver: receiverId,
+    });
+    if (existingRequest) {
+      return NextResponse.json(
+        { message: "درخواست قبلاً ارسال شده است." },
+        { status: 400 }
+      );
+    }
+
+    // save requet in db
+    await FriendRequest.create({ sender: senderId, receiver: receiverId });
+
+    return NextResponse.json({ message: "درخواست دوستی ارسال شد!" });
+  } catch (error) {
+    console.error("خطا در ثبت‌نام:", error);
+    return NextResponse.json({ error: "مشکلی رخ داده است." }, { status: 500 });
+  }
+}
